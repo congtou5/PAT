b75b210a106e1f91be05bb7f96a510727ea7775b
huang huacong <1160247418@qq.com>
2017-02-09 13:49:53 +0800

    1003

---
+++ BasicLevel/1003/1003.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void judge(char s[]);
+
+int main()
+{
+	int n;
+	char str[];
+	scanf("%d\n",&n);
+	for(int i=0; i<n; i++) {
+		scanf("%s\n",&str);
+		judge(str);
+	}
+
+	return 0;
+}
+
+void judge(char s[])
+{
+	
+}
